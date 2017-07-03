5a173e9713b5ebec09c6dd2ea5c6ad033140f4ba
Kanaga2 <priyakanaga112@gmail.com>
2017-07-03 21:29:46 +0530

    Create Length.java

---
+++ Length.java
@@ -0,0 +1,15 @@
+# Second1
+import java.io.*;
+import java.util.*;
+public class Length
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+//System.out.println("Enter a string");
+String a="anna";
+System.out.println("String length is:"+a.length());
+}
+}
+
+
